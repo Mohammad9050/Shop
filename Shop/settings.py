0c5d696ca9a22380cb56e2813e7bcde35137c980
Mohammad9050 <fifa19.900t@gmail.com>
2021-08-18 06:24:52 -0700

    add smpt info to setting and email worked!

--- Shop/settings.py
+++ Shop/settings.py
@@ -129,4 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fifa19.900t@gmail.com'
+EMAIL_HOST_PASSWORD = 'Mohamad01'
